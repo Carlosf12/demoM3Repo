a6b4b6917af4c99276979725ed176525bf36128a
carlos <carlos.figueira1212@gmail.com>
2024-10-03 15:00:19 -0400

    HW 3 Done Update

--- back/src/routes/indexRouter.ts
+++ back/src/routes/indexRouter.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import userRouter from "./userRouter";
 import appointmentRouter from "./appointmentRouter";
 
