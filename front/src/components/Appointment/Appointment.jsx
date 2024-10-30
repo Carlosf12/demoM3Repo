ca2a0240137fb324e7e95a196e2be4140b3d5953
carlos <carlos.figueira1212@gmail.com>
2024-10-30 12:24:56 -0400

    CSS Updates

--- front/src/components/Appointment/Appointment.jsx
+++ front/src/components/Appointment/Appointment.jsx
@@ -3,9 +3,9 @@ import styles from './Appointment.module.css'
 const Appointment = ({ date , time, status, handleCancelApp}) => {
     return (
         <div className={styles.container}>
-            <p>Fecha: {date}</p>
-            <p>Hora: {time}</p>
-            <p>Status: {status}</p>
+            <p className={styles.date}>Fecha: {date}</p>
+            <p className={styles.time}>Hora: {time}</p>
+            <p className={styles.status}>Status: {status}</p>
             <button onClick={handleCancelApp}>Cancelar</button>
         </div>
     )
